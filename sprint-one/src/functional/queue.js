f65c6c3fdc1e5a1aa3cc9f51c46d8df9d8b51236
Remy Orans <remy.orans@gmail.com>
2020-11-18 12:30:23 -0800

    Implement stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,51 @@
 var Queue = function() {
-  var someInstance = {};
+  var someInstance = {
+    count: 0
+  };
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {
+    value: undefined,
+    next: undefined,
+    previous: undefined
+  };
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    //list is zero
+    if (someInstance.count === 0) {
+      storage.value = value;
+    } else {
+      storage.previous = {
+        value: value,
+        next: storage,
+        previous: undefined
+      };
+    }
+
   };
 
   someInstance.dequeue = function() {
+    if (someInstance.count === 1) {
+      let currentValue = storage.value;
+      storage = {
+        value: undefined,
+        next: undefined,
+        previous: undefined
+      };
+      someInstance.count--;
+      return currentValue;
+    } else if (someInstance.count > 1) {
+      let currentValue = storage.value;
+      storage = storage.previous;
+      storage.next = undefined;
+      return currentValue;
+    }
   };
 
   someInstance.size = function() {
+    return someInstance.count;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,51 @@
 var Stack = function() {
-  var someInstance = {};
-
+  var someInstance = {
+    count: 0
+  };
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {
+    value: undefined,
+    next: undefined,
+    previous: undefined
+  };
 
   // Implement the methods below
   someInstance.push = function(value) {
+    someInstance.count++;
+    // first
+    if (someInstance.count === 0) {
+      storage.value = value;
+    } else {
+      storage.next = {
+        value: value,
+        next: undefined,
+        previous: storage
+      };
+      storage = storage.next;
+    }
+
   };
 
   someInstance.pop = function() {
+    if (someInstance.count > 1) {
+      let currentValue = storage.value;
+      storage = storage.previous;
+      storage.next = undefined;
+      someInstance.count--;
+      return currentValue;
+    } else if (someInstance.count === 1) {
+      let currentValue = storage.value;
+      storage.value = undefined;
+      storage.next = undefined;
+      storage.previous = undefined;
+      someInstance.count--;
+      return currentValue;
+    }
+
   };
 
   someInstance.size = function() {
+    return someInstance.count;
   };
 
   return someInstance;
